dc9fa29436d4b0a8f3248f1a9fc25694b29e3b36
Nikolaus Bates-Haus <nbateshaus@gmail.com>
2026-05-23 12:22:41 -0400

    feat: basic edges

---
+++ _targets_r/targets/edge_classes.R
@@ -0,0 +1,10 @@
+tar_target(
+  edge_classes,
+  tribble(
+    ~ID, ~category,
+    1, "Edge",
+    2, "Transition",
+    3, "Interior"
+  )
+)
+
--- _targets_r/targets/edge_distance.R
+++ _targets_r/targets/edge_distance.R
@@ -1,12 +1,16 @@
 tar_terra_rast(
   edge_distance,
   {
-    # terra::distance fills NA pixels with the distance to the nearest
-    # non-NA pixel. Remove forest pixes; these will be filled in by
-    # distance.
     lc4 |>
-      `levels<-`(lc4_classes) |>
-      filter(category != "Forest") |>
-      terra::distance()
+      # target = 4 = Forest
+      # exclude = 1 = Water
+      terra::distance(target = 4, exclude = 1) |>
+      filter(category > 0) |>
+      mutate(category = case_when(
+        category <= 30  ~ 1,
+        category <= 100 ~ 2,
+        category  > 100 ~ 3
+      )) |>
+      terra::mask(lc4)
   }
 )
